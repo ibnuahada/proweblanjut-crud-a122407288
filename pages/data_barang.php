7c176c3b45c97c59ae03f849b92d72ff90bd5bfe
ibnuahada <ibnuahaday@gmail.com>
2026-03-12 09:05:12 +0700

    Membuat operasi READ (Menampilkan data barang)

---
+++ pages/data_barang.php
@@ -0,0 +1,41 @@
+<?php
+include "../koneksi.php";
+
+$query = $conn->query("SELECT * FROM barang");
+$data = $query->fetchAll();
+?>
+
+<table border="1">
+<tr>
+    <th>No</th>
+    <th>Kode Barang</th>
+    <th>Nama Barang</th>
+    <th>Jumlah</th>
+    <th>Harga</th>
+</tr>
+
+<?php if(count($data) > 0): ?>
+
+<?php $no = 1; foreach($data as $row) {?>
+
+<tr>
+    <td><?php echo $no++;?></td>  
+    <td><?php echo $row['kode_barang']; ?></td>
+    <td><?php echo $row['nama_barang']; ?></td>
+    <td><?php echo $row['jumlah']; ?></td>
+    <td>
+        <span>
+        Rp<?php echo number_format($row['harga'], 0, ',', '.'); ?>
+        </span>
+    </td>
+</tr>
+
+<?php } ?>
+<?php else: ?>
+
+<tr>
+    <td colspan="5"> Data Kosong </td>
+</tr>
+
+<?php endif; ?>
+</table>
