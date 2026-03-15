0c80337d0d1279dc89e207af6d4d8d13c366ba72
ibnuahada <ibnuahaday@gmail.com>
2026-03-15 22:14:18 +0700

    fix: memperbaiki path program

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include "includes/header.php";
+?>
+
+<div class="main-layout">
+
+<?php include "includes/menu.php"; ?>
+
+<div class="content">
+
+<?php
+
+$page = $_GET['page'] ?? 'data_barang';
+
+if($page == "data_barang"){
+include "pages/data_barang.php";
+}
+elseif($page == "tambah"){
+include "pages/tambah.php";
+}
+elseif($page == "edit"){
+include "pages/edit.php";
+}
+
+?>
+
+</div>
+</div>
+
+<?php include "includes/footer.php"; ?>
