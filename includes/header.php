0f2e6ae23a26d1392a695d0944f749e8f0ce2ed7
ibnuahada <ibnuahaday@gmail.com>
2026-03-15 22:16:10 +0700

    feat: membuat UI (antarmuka pengguna)

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistem Inventory Barang</title>
+
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1><i class="fa-solid fa-boxes-stacked"></i>Sistem Inventory Barang</h1>
+        <div>
+            <i class="fa-solid fa-user"></i> Admin
+        </div>
+    </div>
+
+    <div class="container">
---
+++ includes/menu.php
@@ -0,0 +1,15 @@
+<div class="menu">
+
+<a href="index.php?page=data_barang">
+<i class="fa-solid fa-box"></i> Data Barang
+</a>
+
+<a href="index.php?page=tambah">
+<i class="fa-solid fa-plus"></i> Tambah Barang
+</a>
+
+<a href="#">
+<i class="fa-solid fa-right-from-bracket"></i> Logout
+</a>
+
+</div>
